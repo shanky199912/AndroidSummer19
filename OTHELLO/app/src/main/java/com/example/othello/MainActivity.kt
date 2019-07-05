9dd3b358f6112f17521cfd76c19e74ba288dd7ba
shanky199912 <shanky199912@outlook.com>
2019-07-05 14:57:08 +0530

    review dynamic buttons

--- OTHELLO/app/src/main/java/com/example/othello/MainActivity.kt
+++ OTHELLO/app/src/main/java/com/example/othello/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_main)
+         setContentView(R.layout.activity_main)
 
         buttonArray = Array(8) {
 
