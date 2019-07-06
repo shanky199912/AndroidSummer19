73a556997a8ff1d8e0f898348dc8ee41c5c24b0e
shanky199912 <shanky199912@outlook.com>
2019-07-06 09:33:37 +0530

    Button successfully added using code

--- OTHELLO/app/src/main/java/com/example/othello/MainActivity.kt
+++ OTHELLO/app/src/main/java/com/example/othello/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.othello
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
-import androidx.core.view.marginLeft
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    lateinit var ll: LinearLayout
-    lateinit var ll1: LinearLayout
+    private lateinit var ll: LinearLayout
+    private lateinit var ll1: LinearLayout
+    private lateinit var blackTv: TextView
+    private lateinit var whiteTv: TextView
+    private lateinit var ll2: LinearLayout
+    private lateinit var ll3: LinearLayout
 
     var button = arrayOfNulls<Array<Button>?>(64)
     private var llayout = arrayOfNulls<LinearLayout?>(8)
@@ -36,7 +38,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 Button(this)
             }
         }
-
 
         /* for (i in 0..7) {
              for (j in 0..7) {
@@ -63,13 +64,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
 
+        ll2 = LinearLayout(this)
+        ll2.orientation = LinearLayout.VERTICAL
+
+        ll2.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+
+        ll3 = LinearLayout(this)
+        ll3.orientation = LinearLayout.HORIZONTAL
+
+        ll3.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
 
         ll1 = LinearLayout(this)
         ll1.orientation = LinearLayout.VERTICAL
-
-/*
-        ll.removeAllViews()
-        ll1.removeAllViews()*/
 
         button = Array(8) {
             Array(8) {
@@ -82,12 +95,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             LinearLayout(this)
         }
 
+
         for (i in 0..7) {
 
-            ll1.layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT,1f
-            )
+            llayout[i]?.apply {
+                layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT, 1f
+                )
+                orientation = LinearLayout.VERTICAL
+            }
 
             for (j in 0..7) {
 
@@ -100,29 +117,66 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val params = button[i]!![j].layoutParams as LinearLayout.LayoutParams
                 params.setMargins(6, 6, 0, 0)
                 button[i]!![j].layoutParams = params
+                button[i]!![j].textSize = 35f
 
                 if (button[i]!![j].parent != null) (button[i]!![j].parent as ViewGroup).removeView(button[i]!![j])
-                ll1.addView(button[i]!![j])
+                llayout[i]!!.addView(button[i]!![j])
             }
 
-            if (ll1.parent != null) {
-                (ll1.parent as ViewGroup).removeView(ll1)
+            if (llayout[i]!!.parent != null) {
+                (llayout[i]!!.parent as ViewGroup).removeView(llayout[i])
             }
+            ll.addView(llayout[i])
 
-            ll.addView(ll1)
         }
 
-        setContentView(ll)
+
+        blackTv = TextView(this)
+        blackTv.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT, 1f
+        )
+        blackTv.text = "Black : 0"
+        blackTv.textSize = 30f
+        val param = blackTv.layoutParams as LinearLayout.LayoutParams
+        param.setMargins(10, 20, 20, 20)
+        blackTv.layoutParams = param
+
+        ll3.addView(blackTv)
+
+        whiteTv = TextView(this)
+        whiteTv.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT, 1f
+        )
+        whiteTv.text = "White : 0"
+        whiteTv.textSize = 30f
+        val params2 = whiteTv.layoutParams as LinearLayout.LayoutParams
+        params2.setMargins(20, 20, 10, 20)
+        whiteTv.layoutParams = params2
+
+        ll3.addView(whiteTv)
+
+        ll2.addView(ll3)
+
+        ll2.addView(ll)
 
 
-        /* startGame.setOnClickListener {
+        setContentView(ll2)
 
-             buttonArray[3]!![3].text = "\u2B24"
-             buttonArray[3]!![4].text = "\u26AA"
-             buttonArray[4]!![3].text = "\u26AA"
-             buttonArray[4]!![4].text = "\u2B24"
+        button[3]!![3].text = "\u2B24"
+        button[3]!![4].text = "\u26AA"
+        button[4]!![3].text = "\u26AA"
+        button[4]!![4].text = "\u2B24"
 
-         }*/
+        for (i in 0..7) {
+            for (j in 0..7) {
+
+                buttonArray[i]!![j] = button[i]!![j]
+                buttonArray[i]!![j].setOnClickListener(this)
+            }
+        }
+
     }
 
     override fun onClick(v: View?) {
@@ -131,98 +185,122 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         if (blackTurn) {
 
-            if (checkForValidPlaceBlack() == isSelected.id) {
+            isSelected.text = "\u2B24"
+        } else {
+            isSelected.text = "\u26AA"
+        }
 
-                //set the black color to the button
-                // v.setBackgroundColor(resources.getColor(R.color.))
-            }
+    }
+
+
+    /* startGame.setOnClickListener {
+
+         buttonArray[3]!![3].text = "\u2B24"
+         buttonArray[3]!![4].text = "\u26AA"
+         buttonArray[4]!![3].text = "\u26AA"
+         buttonArray[4]!![4].text = "\u2B24"
+
+     }*/
+}
+
+/*override fun onClick(v: View?) {
+
+    val isSelected = v as Button
+
+    if (blackTurn) {
+
+        if (checkForValidPlaceBlack() == isSelected.id) {
+
+            //set the black color to the button
+            // v.setBackgroundColor(resources.getColor(R.color.))
+        }
+    }
+}
+
+private fun checkForValidPlaceBlack(): Int {
+
+    val id: Int = 0
+
+    var placeBlack = arrayOfNulls<Array<String>?>(64)
+
+    placeBlack = Array(8) {
+        Array(8) {
+            String()
+        }
+    }
+    for (i in 0..7) {
+        for (j in 0..7) {
+
+            placeBlack[i]!![j] = buttonArray[i]!![j].text.toString()
+
         }
     }
 
-    private fun checkForValidPlaceBlack(): Int {
+    if (buttonArray[0]!![0].text == "B") {
 
-        val id: Int = 0
+        for (i in 2..7) {
+            while (buttonArray[1]!![0].text == "W" &&
+                (buttonArray[i]!![0].text == "" || buttonArray[i]!![0].text == "W")
+            ) {
 
-        var placeBlack = arrayOfNulls<Array<String>?>(64)
-
-        placeBlack = Array(8) {
-            Array(8) {
-                String()
-            }
-        }
-        for (i in 0..7) {
-            for (j in 0..7) {
-
-                placeBlack[i]!![j] = buttonArray[i]!![j].text.toString()
-
-            }
-        }
-
-        if (buttonArray[0]!![0].text == "B") {
-
-            for (i in 2..7) {
-                while (buttonArray[1]!![0].text == "W" &&
-                    (buttonArray[i]!![0].text == "" || buttonArray[i]!![0].text == "W")
-                ) {
-
-                    if (buttonArray[i]!![0].text == "") {
-                        return buttonArray[i]!![0].id
-                    } else {
-                        return id
-                    }
+                if (buttonArray[i]!![0].text == "") {
+                    return buttonArray[i]!![0].id
+                } else {
+                    return id
                 }
             }
-
-            return id
         }
 
-        if (buttonArray[0]!![7].text == "B") {
+        return id
+    }
 
-
-            return id
-        }
-
-        if (buttonArray[7]!![0].text == "B") {
-
-
-            return id
-        }
-
-        if (buttonArray[7]!![7].text == "B") {
-
-
-            return id
-        }
-
-        for (i in 1..6) {
-
-            if (buttonArray[i]!![0].text == "B") {
-
-                return id
-            }
-
-            if (buttonArray[i]!![7].text == "B") {
-
-                return id
-            }
-        }
-
-        for (j in 1..6) {
-
-            if (buttonArray[0]!![j].text == "B") {
-
-                return id
-            }
-
-            if (buttonArray[7]!![j].text == "B") {
-
-                return id
-            }
-        }
+    if (buttonArray[0]!![7].text == "B") {
 
 
         return id
-
     }
-}
+
+    if (buttonArray[7]!![0].text == "B") {
+
+
+        return id
+    }
+
+    if (buttonArray[7]!![7].text == "B") {
+
+
+        return id
+    }
+
+    for (i in 1..6) {
+
+        if (buttonArray[i]!![0].text == "B") {
+
+            return id
+        }
+
+        if (buttonArray[i]!![7].text == "B") {
+
+            return id
+        }
+    }
+
+    for (j in 1..6) {
+
+        if (buttonArray[0]!![j].text == "B") {
+
+            return id
+        }
+
+        if (buttonArray[7]!![j].text == "B") {
+
+            return id
+        }
+    }
+
+
+    return id
+
+}*/
+
 
